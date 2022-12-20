f1a9ec91552b1e3123f7feeef69120754394d84a
menulln <39300523+menulln@users.noreply.github.com>
2022-12-20 17:17:42 +0100

    Add drawBoard function to Controller

--- game.js
+++ game.js
@@ -1,7 +1,29 @@
 const Game = (() => {
-	const board = [0, 0, 0, 0, 0, 0, 0, 0, 0];
+	const x = '\u2716';
+	const o = '\u20DD';
+	const board = [x, o, x, x, o, x, o, x, x];
+	const getBoard = () => board;
+	return { getBoard };
 })();
 
-const Controller = (() => {})();
+const Controller = (() => {
+	const drawBoard = () => {
+		const board = Game.getBoard();
+		const squares = document.querySelectorAll('.square');
+		const x = '\u2716';
+		const o = '\u20DD';
+		let i = 0;
+		squares.forEach((square) => {
+			square.textContent = board[i];
+			if (board[i] === x) {
+				square.style.fontWeight = 100;
+			} else if (board[i] === o) {
+				square.style.fontWeight = 1000;
+			}
+			i++;
+		});
+	};
+	return { drawBoard };
+})();
 
 const Player = (marker) => {};
