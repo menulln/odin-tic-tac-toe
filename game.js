cda2662e87775f44034056bf25e744ab8c43a4c4
menulln <39300523+menulln@users.noreply.github.com>
2022-12-20 18:23:32 +0100

    Add functions to start game and place markers

--- game.js
+++ game.js
@@ -1,29 +1,72 @@
+const Player = (marker) => ({ marker });
+
 const Game = (() => {
 	const x = '\u2716';
 	const o = '\u20DD';
-	const board = [x, o, x, x, o, x, o, x, x];
+	const playerOne = Player(x);
+	const playerTwo = Player(o);
+	const board = [0, 0, 0, 0, 0, 0, 0, 0, 0];
 	const getBoard = () => board;
-	return { getBoard };
+	const setBoard = (position, marker) => {
+		board.splice(position - 1, 1, marker);
+	};
+	const init = () => {
+		const squares = document.querySelectorAll('.square');
+		squares.forEach((square) => {
+			square.addEventListener('pointerdown', Controller.placeMarker);
+		});
+		Controller.drawBoard();
+	};
+	const start = () => {
+		init();
+	};
+	return {
+		start,
+		getBoard,
+		setBoard,
+		playerOne,
+		playerTwo,
+		x,
+		o,
+	};
 })();
 
 const Controller = (() => {
+	let playerOneTurn = true;
+
 	const drawBoard = () => {
 		const board = Game.getBoard();
 		const squares = document.querySelectorAll('.square');
-		const x = '\u2716';
-		const o = '\u20DD';
 		let i = 0;
 		squares.forEach((square) => {
 			square.textContent = board[i];
-			if (board[i] === x) {
+			if (board[i] === Game.x) {
 				square.style.fontWeight = 100;
-			} else if (board[i] === o) {
+			} else if (board[i] === Game.o) {
 				square.style.fontWeight = 1000;
 			}
 			i++;
 		});
 	};
-	return { drawBoard };
+	const placeMarker = (e) => {
+		const { position } = e.target.dataset;
+		const marker = playerOneTurn
+			? Game.playerOne.marker
+			: Game.playerTwo.marker;
+		console.log(playerOneTurn);
+		if (playerOneTurn) {
+			Game.setBoard(position, marker);
+			console.log(Game.getBoard());
+			drawBoard();
+			playerOneTurn = false;
+		} else {
+			Game.setBoard(position, marker);
+			drawBoard();
+			playerOneTurn = true;
+		}
+	};
+	return {
+		drawBoard,
+		placeMarker,
+	};
 })();
-
-const Player = (marker) => {};
